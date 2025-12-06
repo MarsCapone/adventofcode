ee1a0b342819cca3d7a93b1f55c02f444ef30db4
Samson Ventura-Danziger <samson@innerworks.me>
2025-12-06 05:38:12 +0000

    merge day 6 to a single file

--- 2025/aoc6.py
+++ 2025/aoc6.py
@@ -5,15 +5,6 @@ from functools import reduce
 with input_file(6) as f:
     inp = [line.strip("\n") for line in f.readlines()]
 
-ops = {'+': add, '*': mul}
-p1 = 0
-
-for calc in zip(*(l.split() for l in inp)):
-    op, *nums = reversed(calc)
-    p1 += reduce(ops[op], map(int, nums))
-
-# -- 
-
 def get_operations_with_spacing():
     operations_line = inp[-1]
     operations = []
@@ -30,22 +21,24 @@ def get_operations_with_spacing():
     operations.append((current_op, space_count + 1))
     return operations
 
-p2 = 0
+OPS = {'+': add, '*': mul}
+p1 = p2 = 0
 
-# get each op and the number of characters to read
-# for each line, read that many characters
-# 3 => ['64 ', '23 ', '314']
-# zip => ['623', '431', '  4']
-# cast to int and apply op
-
-p2 = 0
 start_index = 0
 for op, spacing in get_operations_with_spacing():
     next_nums = [l[start_index:start_index+spacing] for l in inp[:-1]]
     start_index += spacing + 1  # account for the space between columns
 
-    new_nums = (int("".join(z).strip()) for z in zip(*next_nums))
-    p2 += reduce(ops[op], new_nums)
+    p1 += reduce(OPS[op], map(int, next_nums))
+    p2 += reduce(OPS[op], (int("".join(z).strip()) for z in zip(*next_nums)))
+
+
+# P2 plan
+# get each op and the number of characters to read
+# for each line, read that many characters
+# 3 => ['64 ', '23 ', '314']
+# zip => ['623', '431', '  4']
+# cast to int and apply op
 
 print_results(p1, p2)
     
